8f16bbf12342555269e16853ca4c7c478295a397
denizaral <araldeniz97@gmail.com>
2021-01-25 18:25:03 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys;
+            TValue[] tempValues;
+
+            tempKeys = _keys;
+            tempValues = _values;
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                _values[i] = tempValues[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+            Console.Write(_keys[_keys.Length - 1]);
+            Console.WriteLine(_values[_values.Length - 1] + " " + "Eklendi.");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1997, "Deniz");
+            myDictionary.Add(1984, "Engin");
+        }
+    }
+}
